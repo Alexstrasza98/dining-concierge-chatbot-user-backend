3e4ee2c74d7f3e92827393eca8b59a4aab633252
Zixuan Yan <zy2501@columbia.edu>
2022-12-04 20:44:11 -0500

    Add redis settings

--- tasks.py
+++ tasks.py
@@ -1,13 +1,16 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
 
-def send_simple_message(to, subject, body):
+def send_simple_message(to, subject, body, html):
     domain = os.getenv("MAILGUN_DOMAIN")
     return requests.post(
         f"https://api.mailgun.net/v3/{DOMAIN}/messages",
@@ -15,7 +18,12 @@ def send_simple_message(to, subject, body):
         data={"from": f"Zixuan Yan <mailgun@{DOMAIN}>",
               "to": [to],
               "subject": subject,
-              "text": body})
+              "text": body,
+              "html": html})
+
+
+def render_template(template_filename, **content):
+    return template_env.get_template(template_filename).render(**content)
 
 
 def send_user_registration_email(email, username):
@@ -23,6 +31,7 @@ def send_user_registration_email(email, username):
         email,
         "Successfully signed up.",
         f"Hi {username}! You have successfully signed up to the Stores REST API.",
+        render_template("email/action.html", username=username)
     )
 
 
