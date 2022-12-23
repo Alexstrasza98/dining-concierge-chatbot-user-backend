1648c10f6912a5cabca12d460dda92b3b0a5ba28
Zixuan Yan <zy2501@columbia.edu>
2022-12-23 02:53:59 -0500

    a

--- app.py
+++ app.py
@@ -107,6 +107,9 @@ def create_app(db_url=None):
     @app.after_request
     def apply_caching(response):
         response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,OPTIONS,POST,PUT"
+        response.headers["Access-Control-Allow-Headers"] = "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
         return response
 
     return app
